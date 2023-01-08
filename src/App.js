bdb69aaf196df3a71b4bb655778d5eddf016fd86
Konstanrin Mukhin <kostik.mukhin.1995@gmail.com>
2023-01-08 21:28:35 +0300

    adding routing version 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import {
-  BrowserRouter,
+  Navigate,
   NavLink,
-  Redirect,
-  Route,
-  Switch,
+  Outlet,
   useParams,
+  useRoutes,
 } from "react-router-dom";
 
 const users = [
@@ -17,17 +16,34 @@ const users = [
 ];
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/users" component={UserLayout} />
-          <Route path="/" exact component={MainPage} />
-          <Redirect to="/" />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+  const elements = useRoutes([
+    {
+      path: "/users",
+      element: <UserLayout />,
+      children: [
+        { path: "", element: <UsersListPage /> },
+        {
+          path: ":userId",
+          element: <Outlet />,
+          children: [
+            { path: "profile", element: <UserProfilePage /> },
+            { path: "edit", element: <EditUserPage /> },
+            { path: "*", element: <Navigate to={"profile"} /> },
+            { path: "", element: <Navigate to={"profile"} /> },
+          ],
+        },
+      ],
+    },
+    {
+      path: "/",
+      element: <MainPage />,
+    },
+    {
+      path: "*",
+      element: <Navigate to="/" />,
+    },
+  ]);
+  return <div className="App">{elements}</div>;
 }
 
 function MainPage() {
@@ -42,12 +58,7 @@ function MainPage() {
 function UserLayout() {
   return (
     <div>
-      <Switch>
-        <Route path={"/users/:userId/profile"} component={UserProfilePage} />
-        <Route path={"/users/:userId/edit"} component={EditUserPage} />
-        <Route path={"/users"} exact component={UsersListPage} />
-        <Redirect from="/users/:userId" to={"/users/:userId/profile"} />
-      </Switch>
+      <Outlet />
     </div>
   );
 }
@@ -74,17 +85,29 @@ function UserProfilePage() {
   const { userId } = useParams();
   return (
     <div>
-      <h1>User Page №{userId}</h1>
+      <h1>User Page</h1>
       <hr />
-      <h2>mame: {users[userId - 1].name}</h2>
-      <h2>userId: {userId}</h2>
-      <button>
-        <NavLink to={"/users"}>Userslistpage</NavLink>
-      </button>
-      <tr />
-      <button>
-        <NavLink to={`/users/${userId}/edit`}>EditUserPage</NavLink>
-      </button>
+      {users[userId - 1] ? (
+        <>
+          <h2>mame: {users[userId - 1].name}</h2>
+          <h2>userId: {userId}</h2>
+          <button>
+            <NavLink to={"/users"}>Userslistpage</NavLink>
+          </button>
+
+          <button>
+            <NavLink to={`/users/${userId}/edit`}>EditUserPage</NavLink>
+          </button>
+        </>
+      ) : (
+        <>
+          <h2>Пользователя {userId} не существует </h2>
+          <tr />
+          <button>
+            <NavLink to={"/users"}>Userslistpage</NavLink>
+          </button>
+        </>
+      )}
     </div>
   );
 }
@@ -102,11 +125,15 @@ function EditUserPage() {
       </button>
       <tr />
       <button>
-        <NavLink to={`/users/${userId}`}>UserProfilePage</NavLink>
+        <NavLink to={"/users/" + userId + "/profile"}>UserProfilePage</NavLink>
       </button>
       <tr />
       <button>
-        <NavLink to={`/users/${users.length <= userId ? 1 : +userId + 1}`}>
+        <NavLink
+          to={
+            "/users/" + (users.length <= userId ? 1 : +userId + 1) + "/profile"
+          }
+        >
           Another user page
         </NavLink>
       </button>
